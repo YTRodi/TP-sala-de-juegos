cef1aec924467c5bee1dd52ee4a659fe856189ae
YTRodi <yagorodi@gmail.com>
2021-03-28 01:59:58 -0300

    material divider added

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 
 import { MatButtonModule } from '@angular/material/button';
+import { MatDividerModule } from '@angular/material/divider';
 import { MatListModule } from '@angular/material/list';
 import { MatIconModule } from '@angular/material/icon';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
@@ -10,6 +11,7 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 @NgModule({
   exports: [ // Lo exporto para ofrecerselo a otro módulo que use angular material.
     MatButtonModule,
+    MatDividerModule,
     MatListModule,
     MatIconModule,
     MatSlideToggleModule,
