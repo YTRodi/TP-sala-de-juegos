c1fc477a51cdbad26426298a0fe23b29008f931c
YTRodi <yagorodi@gmail.com>
2021-03-29 16:16:48 -0300

    about component almost finished

--- src/app/protected/pages/about/about.component.ts
+++ src/app/protected/pages/about/about.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Icon } from './interfaces/icon';
 
 @Component({
   selector: 'app-about',
@@ -6,6 +7,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./about.component.css']
 })
 export class AboutComponent implements OnInit {
+  
+  public listIcons: Icon[]  = [
+    { src: '../../../../assets/icons/html.svg', alt: 'HTML icon' },
+    { src: '../../../../assets/icons/css.svg', alt: 'CSS icon' },
+    { src: '../../../../assets/icons/javaScript.svg', alt: 'JavaScript icon' },
+    { src: '../../../../assets/icons/mongoDB.svg', alt: 'mongoDB icon' },
+    { src: '../../../../assets/icons/express.svg', alt: 'Express icon' },
+    { src: '../../../../assets/icons/react.svg', alt: 'React icon' },
+    { src: '../../../../assets/icons/nodeJS.svg', alt: 'nodeJS icon' },
+    { src: '../../../../assets/icons/redux.svg', alt: 'Redux icon' },
+    { src: '../../../../assets/icons/webpack.svg', alt: 'Webpack icon' },
+    { src: '../../../../assets/icons/angular.svg', alt: 'Angular icon' }
+  ]
 
   constructor() { }
 
