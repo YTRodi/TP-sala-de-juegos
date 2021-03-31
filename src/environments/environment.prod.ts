ebdb960bcde7ca4f60a063965735adb00202ef5f
YTRodi <yagorodi@gmail.com>
2021-03-31 11:40:12 -0300

    firebase config added

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyDjHPrID0NkLoOYkh_ESXxOFDK_RnT1jl8",
+    authDomain: "tp-sala-de-juegos-9e127.firebaseapp.com",
+    projectId: "tp-sala-de-juegos-9e127",
+    storageBucket: "tp-sala-de-juegos-9e127.appspot.com",
+    messagingSenderId: "166337164284",
+    appId: "1:166337164284:web:0da931e57d6e610a3cb879",
+    measurementId: "G-2NJ4LN92H2"
+  }
 };
