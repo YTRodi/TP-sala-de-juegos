8f79bd05f3b2be855fb783bc8b4338f88193ab98
Yago Rodi <yagorodi@gmail.com>
2021-03-31 12:37:57 -0300

    Feature/agregar y configurar sdk firebase (#7)
    * Develop (#4)
    
    * set exports in NgModule
    
    * angular material and angular flex layout added
    
    * FlexLayoutModule added in protected module
    
    * material module added in ProtectedModule
    
    * sidebar using angular material added
    
    * favicon added
    
    * route for home added
    
    * material divider added
    
    * Feature/about page (#2)
    
    * icons added
    
    * img profile added
    
    * Icon interface added
    
    * about component almost finished
    
    * Bugfix/routing home (#3)
    
    * dashboard component added
    
    * .gitignore updated
    
    * sdk firebase added
    
    * firebase config added
    
    * angular fire added
    
    * AngularFire and AngularFirestore modules added
    
    * auth service added

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyDjHPrID0NkLoOYkh_ESXxOFDK_RnT1jl8",
+    authDomain: "tp-sala-de-juegos-9e127.firebaseapp.com",
+    projectId: "tp-sala-de-juegos-9e127",
+    storageBucket: "tp-sala-de-juegos-9e127.appspot.com",
+    messagingSenderId: "166337164284",
+    appId: "1:166337164284:web:0da931e57d6e610a3cb879",
+    measurementId: "G-2NJ4LN92H2"
+  }
 };
 
 /*
