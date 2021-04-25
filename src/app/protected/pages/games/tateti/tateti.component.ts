46a7196b1f587f925ee09a92c2d70986bb1b50a8
YTRodi <yagorodi@gmail.com>
2021-04-25 20:52:42 -0300

    refactor of chats

--- src/app/protected/pages/games/tateti/tateti.component.ts
+++ src/app/protected/pages/games/tateti/tateti.component.ts
@@ -3,13 +3,11 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-tateti',
   templateUrl: './tateti.component.html',
-  styleUrls: ['./tateti.component.css']
+  styleUrls: ['./tateti.component.css'],
 })
 export class TatetiComponent implements OnInit {
+  // TODO: Empezar con el tateti!
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
